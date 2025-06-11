b64af9c72c16b6899fbb258ed1b62b27643eefce
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-06-11 12:09:31 +0500

    mission done

---
+++ task02.py
@@ -0,0 +1,9 @@
+number = (input("Raqamni kiriting: "))
+
+result = number[:1]
+result02 = number[1:2]
+result03 = number[2:3]
+
+result01 = (result)+(result02)+(result03)
+
+print(f"{result}+{result02}+{result03}={result01}")
