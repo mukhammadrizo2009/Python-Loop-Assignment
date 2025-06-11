36e9b76984db765533da032cd26aac8b98df4f80
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-06-11 11:53:14 +0500

    Mission Done

---
+++ task01.py
@@ -0,0 +1,11 @@
+text = input("Matn kiriting: ")
+
+letter = "a,o,e,i,o,u"
+
+hisob = 0
+
+for i in text:
+    if i in letter:
+        hisob += 1
+        result = hisob
+        print(f"Harflar soni:",result)
